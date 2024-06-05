2719c31aa1bce352f38689e6c4781c78257ed17a
JoaoMatheus27 <165078857+JoaoMatheus27@users.noreply.github.com>
2024-06-05 16:39:41 -0300

    Delete primeiro_projeto.java directory

--- primeiro_projeto.java/Perfil.java
+++
@@ -1,9 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Perfil {
-    public static void main(String[] args) {
-        System.out.printf("Olá João");
-        System.out.printf("tudo bem?");
-
-    }
-}
